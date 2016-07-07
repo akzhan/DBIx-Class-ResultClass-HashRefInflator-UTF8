fa6e5d3814cb614db4097adf8936cc520e3005cf
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2016-07-07 14:44:23 +0300

    minor

--- lib/DBIx/Class/ResultClass/HashRefInflator/UTF8.pm
+++ lib/DBIx/Class/ResultClass/HashRefInflator/UTF8.pm
@@ -9,14 +9,15 @@ use parent qw( DBIx::Class::ResultClass::HashRefInflator );
 sub inflate_result {
     my ( $self, @args ) = @_;
     my $res =  $self->SUPER::inflate_result( @args );
-    $res = {
+    return $res  if ref($res) ne 'HASH';
+
+    return {
         map {
             my $val = $res->{$_};
             utf8::decode($val)  if defined $val && !ref($val);
             ( $_ => $val );
         } keys %$res
-    }  if ref($res) eq 'HASH';
-    return $res;
+    };
 }
 
 1;
